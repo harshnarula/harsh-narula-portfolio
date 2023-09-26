e53d97d5c77d55aa70ec9487eb7b894c8980015d
harshnarula <91422112+harshnarula@users.noreply.github.com>
2023-09-26 22:56:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import Navbar from './components/Navbar';
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Project from './components/Project';
+import Social from './components/Social';
+import Contact from './components/Contact';
+
+import './App.css';
+
+
+function App() {
+
+  const [social, setSocial] = useState({
+    linkedIn: false,
+    github: false,
+    twitter: false,                                                         
+    gmail: false,
+    instagram: false,
+  })
+
+  const [projects, setProjects] = useState([]);
+
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const [width, setWidth] = useState(window.innerWidth);
+  
+
+  return (
+    
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home
+            width = {width}
+            setWidth = {setWidth}
+          
+          />} />
+          <Route path="/social" element={<Social
+
+              social = {social}
+              setSocial = {setSocial}
+              setWidth = {setWidth}
+
+          />} />
+          <Route path="/projects" element={<div><Project 
+              projects = {projects}
+              setProjects = {setProjects}
+              width = {width}
+              setWidth = {setWidth}
+
+          />
+          </div>} />
+
+        </Routes>
+        <Contact contact ={contact} setContact = {setContact}/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
